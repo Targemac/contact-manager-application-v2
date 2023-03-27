3b13135ace619458e64a7034cdd250da778f8c4a
Targemac <targemac@gmail.com>
2023-03-27 12:55:00 +0100

    built navbar and footer

--- client/src/components/Partials/Footer/Footer.js
+++ client/src/components/Partials/Footer/Footer.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
+import {  FaUserShield, FaUserPlus } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="main-footer-wrapper">
+      <div className="link-grp">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+        <Link className="nav-login-btn" to="/login">
+          Login
+          <FaUserShield />
+        </Link>
+        <Link className="nav-signup-btn" to="/signup">
+          SignUp
+          <FaUserPlus />
+        </Link>
+      </div>
+      <div className="copyright-box">
+        &copy; Copyright 2023 CMA, All rights reserved.
+      </div>
+    </footer>
   );
 };
 
--- client/src/components/Partials/Header/Header.js
+++ client/src/components/Partials/Header/Header.js
@@ -1,11 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
+// import headStyle from "./Header.Modules.css";
+import logo from "../../../assets/CMA-v2.svg";
+import { FaBars, FaUserShield, FaUserPlus } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <nav className="main-navbar" >
+      <div className="logo-box">
+        <Link to="/">
+          <img src={logo} alt="CMA Logo" />
+        </Link>
+      </div>
+      <div className="nav-link-grp">
+        <div className="nav-toggler">
+          <span id="toggleBtn">
+            <FaBars />
+          </span>
+        </div>
+        <div className="link-grp">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/contact">Contact</Link>
+          <Link className="nav-login-btn" to="/login">
+            Login
+            <FaUserShield />
+          </Link>
+          <Link className="nav-signup-btn" to="/signup">
+            SignUp
+            <FaUserPlus />
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
